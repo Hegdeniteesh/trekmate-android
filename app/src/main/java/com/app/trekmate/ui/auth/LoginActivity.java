d9f634f948cbbd087d673f4cf6b740bf2c5c7883
Hegdeniteesh <niteeshhegde1721@gmail.com>
2026-01-05 23:18:32 +0530

    add modern ui & login activity

---
+++ app/src/main/java/com/app/trekmate/ui/auth/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.app.trekmate.ui.auth;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+import com.app.trekmate.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+    }
+}
