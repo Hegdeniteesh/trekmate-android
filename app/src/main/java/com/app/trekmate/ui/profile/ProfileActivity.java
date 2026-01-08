642e0c736ee9a61b221ab1034e0f50a4f5f04c1f
Hegdeniteesh <niteeshhegde1721@gmail.com>
2026-01-08 23:25:09 +0530

    add user profile screen with Firestore view and update

---
+++ app/src/main/java/com/app/trekmate/ui/profile/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.app.trekmate.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.app.trekmate.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
